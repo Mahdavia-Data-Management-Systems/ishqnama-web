7fae7af9215f5042e6e2d365a9c87c3662620250
noormahdi <noormahdi@live.com>
2026-08-18 13:56:15 +1200

    Refactor CI workflow: remove unused job and inputs for production deployment

--- infra/environments/dev/outputs.tf
+++ infra/environments/dev/outputs.tf
@@ -1,14 +1,14 @@
-output "container_app_url" {
-  description = "URL of the deployed Container App"
-  value       = module.aca.container_app_url
-}
+# output "container_app_url" {
+#   description = "URL of the deployed Container App"
+#   value       = module.aca.container_app_url
+# }
 
-output "container_app_fqdn" {
-  description = "FQDN of the deployed Container App"
-  value       = module.aca.container_app_fqdn
-}
+# output "container_app_fqdn" {
+#   description = "FQDN of the deployed Container App"
+#   value       = module.aca.container_app_fqdn
+# }
 
-output "environment_default_domain" {
-  description = "Default domain of the Container Apps environment"
-  value       = module.aca.environment_default_domain
-}
+# output "environment_default_domain" {
+#   description = "Default domain of the Container Apps environment"
+#   value       = module.aca.environment_default_domain
+# }
