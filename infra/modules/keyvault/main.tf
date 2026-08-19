70007bc8fbaa115f8506bae08c5b63d25c3b2b79
noormahdi <noormahdi@live.com>
2026-08-19 15:50:57 +1200

    feat: add GitHub workflow for destroying infrastructure with confirmation input

--- infra/modules/keyvault/main.tf
+++ infra/modules/keyvault/main.tf
@@ -1,12 +1,13 @@
 data "azurerm_client_config" "current" {}
 
 resource "azurerm_key_vault" "this" {
-  name                = var.name
-  resource_group_name = var.resource_group_name
-  location            = var.location
-  tenant_id           = data.azurerm_client_config.current.tenant_id
-  sku_name            = "standard"
-  tags                = var.tags
+  name                       = var.name
+  resource_group_name        = var.resource_group_name
+  location                   = var.location
+  tenant_id                  = data.azurerm_client_config.current.tenant_id
+  sku_name                   = "standard"
+  rbac_authorization_enabled = false
+  tags                       = var.tags
 
   access_policy {
     tenant_id = data.azurerm_client_config.current.tenant_id
