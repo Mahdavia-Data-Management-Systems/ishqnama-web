99a3285a5eb957d20b1e39edbf5df06d51a149e1
noormahdi <noormahdi@live.com>
2026-08-19 19:39:14 +1200

    terraform fmt

--- infra/environments/dev/main.tf
+++ infra/environments/dev/main.tf
@@ -22,7 +22,7 @@ module "swa" {
   tags                = local.tags
 
   app_settings = {
-    NEXT_PUBLIC_ENTRA_AUTHORITY  = "https://${split(".", data.tfe_outputs.mdms-core.values.tenant_domain)[0]}.ciamlogin.com/${data.tfe_outputs.mdms-core.values.tenant_id}"
+    NEXT_PUBLIC_ENTRA_AUTHORITY = "https://${split(".", data.tfe_outputs.mdms-core.values.tenant_domain)[0]}.ciamlogin.com/${data.tfe_outputs.mdms-core.values.tenant_id}"
     NEXT_PUBLIC_ENTRA_CLIENT_ID = data.tfe_outputs.apps-dev.values.apps["ishqnama-spa"].app_id
   }
 }
